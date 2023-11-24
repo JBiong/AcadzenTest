29f2c7fcbe1b6632bb4b5c378a86375e9161751a
Rovelyn5 <rovelynaguinaldo01@gmail.com>
2023-11-24 10:47:17 +0800

    update, balikon ra ni nako later huhu

--- src/UploadDocument.js
+++ src/UploadDocument.js
@@ -2,18 +2,47 @@ import React from "react";
 import "./UploadDocument.css";
 import { AppBar, Toolbar, Typography, IconButton, Box, Button } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import { useState } from 'react';
+
 
 function UploadDocument() {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleBrowseClick = () => {
+        document.getElementById('fileInput').click();
+      };
+    
+      const handleFileChange = (e) => {
+        const file = e.target.files[0];
+    
+        if (file) {
+          // Set the selected file for preview
+          setSelectedFile(file);
+        }
+      };
+    
+      const handleUploadClick = () => {
+        // Implement your upload logic here
+        console.log('Upload button clicked');
+      };
+
+      const getFileType = (fileName) => {
+        const extension = fileName.split('.').pop().toLowerCase();
+        return extension;
+      };
+    
     return (
     <>
     <div className="welcome-back-page">
+    <div className="lsbody">
         <AppBar style={{background: 'none', boxShadow: 'none', padding: '10px', marginTop: '20px'}}>
             <Toolbar>
                 <img src= "/logo.png" alt="App Logo" style={{width: 100, marginLeft: '50px'}}/>
                 <Typography variant="h3" style={{fontFamily: 'Poppin, sans-serif', fontWeight: '600', fontSize: '40px',color: '#B18A00'}}
                 >AcadZen
                 </Typography>
-                <div style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '10px', marginLeft: '100px'}}>
+                <div style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '10px', marginLeft: '50px'}}>
                     <div style={{ background: 'white', borderRadius: '15px', textAlign: 'center', height: '55px', width: '1101px', boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}}>
                         <Typography variant="h4" style={{ fontFamily: "Roboto Condensed", fontSize: '35px',color: '#332D2D', textAlign: 'center' }}
                         >Document to Flashcard Converter
@@ -31,23 +60,84 @@ function UploadDocument() {
         </AppBar>
 
         <div className="upload-document-content">
-            {/* Left Panel for Browsing/Uploading Files */}
+            
             <div className="left-panel">
-                <img src= "/document icon.png" alt="Document Icon" style={{width: 100, marginTop: '120px', marginBottom: '30px'}}/>
-                <Typography variant="h4" style={{ fontFamily: 'Roboto Condensed', fontSize: '20px', color: 'black', textAlign: 'center', fontWeight: 'bold'}}>
-                    Supported document types: PDF, DOCX, TXT, PPTX
-                </Typography>
-                <div style={{marginTop: '60px'}}>
-                <Button style={{background: '#FAC712', width: '230px', height: '45px', borderRadius: '10px'}}> 
-                    <Typography style={{fontSize: '20px', fontFamily: 'Nunito Sans, sans-serif', fontWeight: 'bold', color: '#332D2D', textTransform: 'none'}}>
-                        Browse 
-                    </Typography> 
-                </Button>
+            {/* File preview or document icon */}
+            {selectedFile ? (
+                <div style={{ marginBottom: '10px', width: '100%'}}>
+                {getFileType(selectedFile.name) === 'pdf' ? (
+                    <embed src={URL.createObjectURL(selectedFile)} type="application/pdf" width="40%" height="300px"/>
+                ) : getFileType(selectedFile.name) === 'docx' ? (
+                    // Displaying a placeholder icon for DOCX files
+                    <img
+                    src="/docx-icon.png"
+                    alt="DOCX Icon"
+                    style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
+                    />
+                ) : (
+                    <img
+                    src="/document icon.png"
+                    alt="File Preview"
+                    style={{ width: '70%', maxHeight: '100px', objectFit: 'cover', borderRadius: '10px' }}
+                    />
+                )}
                 </div>
-                <Button style={{background: '#FAC712', width: '230px', height: '45px', borderRadius: '10px', marginTop: '170px'}}> 
-                    <Typography style={{fontSize: '20px', fontFamily: 'Nunito Sans, sans-serif', fontWeight: 'bold', color: '#332D2D', textTransform: 'none'}}>
-                        Upload Document 
-                    </Typography> 
+            ) : (
+                <img
+                src="/document icon.png"
+                alt="Document Icon"
+                style={{ width: 100, marginTop: '130px', marginBottom: '30px' }}
+                />
+            )}
+
+            {/* Display file name or Supported document types */}
+            {selectedFile ? (
+                <Typography
+                variant="h4"
+                style={{
+                    fontFamily: 'Roboto Condensed',
+                    fontSize: '20px',
+                    color: 'black',
+                    textAlign: 'center',
+                    fontWeight: 'bold',
+                    fontStyle: 'italic',
+                    marginTop: '15px',
+                }}
+                >
+                {selectedFile.name}
+                </Typography>
+            ) : (
+                <Typography
+                variant="h4"
+                style={{
+                    fontFamily: 'Roboto Condensed',
+                    fontSize: '20px',
+                    color: 'black',
+                    textAlign: 'center',
+                    fontWeight: 'bold',
+                    marginTop: '10px',
+                }}
+                >
+                Supported document types: PDF, DOCX, TXT, PPTX
+                </Typography>
+            )}
+
+            {/* Browse Button */}
+            <div style={{ marginTop: '70px'}}>
+                <Button style={{ background: '#FAC712', width: '230px', height: '45px', borderRadius: '10px' }} onClick={handleBrowseClick}>
+                <Typography style={{ fontSize: '20px', fontFamily: 'Nunito Sans, sans-serif', fontWeight: 'bold', color: '#332D2D', textTransform: 'none' }}>
+                    Browse
+                </Typography>
+                </Button>
+                {/* Hidden file input */}
+                <input type="file" id="fileInput" style={{ display: 'none' }} onChange={handleFileChange} />
+            </div>
+
+            {/* Upload Document Button */}
+                <Button style={{ background: '#FAC712', width: '230px', height: '45px', borderRadius: '10px', marginTop: '150px' }} onClick={handleUploadClick}>
+                <Typography style={{ fontSize: '20px', fontFamily: 'Nunito Sans, sans-serif', fontWeight: 'bold', color: '#332D2D', textTransform: 'none' }}>
+                    Upload Document
+                </Typography>
                 </Button>
             </div>
 
@@ -58,6 +148,7 @@ function UploadDocument() {
             </Typography>
             {/* Add your file display components here */}
             </div>
+        </div>
         </div>
     </div>
     </>
