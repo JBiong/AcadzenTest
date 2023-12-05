7afe268cf5e08a2e9379d17fbcee941bb59f36c2
Joshua Biong <joshua.biong@cit.edu>
2023-12-05 14:31:24 +0800

    index

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <HomePage/> */}
+    <HomePage/>
     {/* <Dashboard/> */}
     {/* <UploadDocument/> */}
     {/* <FlashcardProgress/> */}
-    <LearningSession/>
+    {/* <LearningSession/> */}
     {/* <AboutAcadZen/> */}
     {/* <AboutUs/> */}
   </React.StrictMode>
